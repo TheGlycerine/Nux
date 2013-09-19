a39e4802d330194f5b5125fcad12914dd7701051
Jay Jagpal <jay@strangemother.com>
2013-09-19 23:07:42 +0100

    use the Nux kernel
    Implementing a Kernel file allows for all the required parts of Nux to
    be removed from a users file and implemented into a repo file.

--- src/js/example.js
+++ src/js/example.js
@@ -1,39 +1,6 @@
 Nux.onReady(function(Nux){
-	 
-	Nux.addAllowed([
-		"nux.extension.core", 
-		/*{ 
-			name: "nux.extension.timer", 
-			// List of allows overrides this
-			// extension can extend
-			overrides: ['nux.extension.listener']
-		},*/
-		"nux.extension.timer",
-		"nux.extension.error",
-		"nux.extension.loader",
-		"nux.extension.logger",
-		"nux.extension.packager",
-		"nux.extension.alpha.signals"
-	]);
-
-	Nux.use(['core', 'timer'], function(extension){
-		console.log("Core and timer imported")
-	}).then('loader', function(extension){
-		// stack overload.
-		if(cc>10) { cc++; debugger;  };
-		
-		// Wait for all expected packages (as required via the imported packages)
-		// to import.
-		Nux.onAllExpected(function(){ 
-			// This should only be run once and 
-			// complete is defined by the extensions
-			// loaded in this chain.
-			console.timeEnd('Full load')
-			console.timeEnd('Nux')
-			Nux.core.slog('FINISH', 'Nux');
-		})
-	});
-
+	Nux.addAllowed(['nux.extension.kernal']).use('kernal', function() {
+		Nux.slog("Kernal", 'setup')
+	})
 });
 
-var cc = 0;
