b421ce7b1dfbe13b3cecfd994fb245a2fbc86f29
Jay Jagpal <jay@strangemother.com>
2013-10-29 01:17:41 +0000

    Add buttons to perform imports

--- examples/js/example_a.js
+++ examples/js/example_a.js
@@ -1,10 +1,21 @@
-Nux
+$(document).ready(function(){
+	
+	$('button.import').click(function(){
+		var self = $(this);
+		Nux.use(self.text(), function(){
+			self.addClass('imported')
+		})
+	})
+
+})
+
+;Nux
 // First the boot, applying a set of rules to
 // boot your nux object.
 .boot({
 	// Boot a full Nux system complete with batteries.
 	// refer to nux.extension.kernel
-	kernel: true,
+	kernel: false,
 	// console logs and test are implemented if required.
 	debug: true,
 	assetPath: '../src/',
@@ -12,18 +23,14 @@ Nux
 	// {extennsionNamespace}.{extensionName}.js
 	extensionPath: "../src/js/nux/extensions/",
 	vendorPath: "../src/js/nux/vendor/",
-
+	required: false,
 	// Extensions allowed to be executed and implemenred
 	// This should only exist in
 	// core loaders and baked
 	// objects
 	allowed: [
 	],
-	ignoreLog: [ 'wanted', 
-	'space', 
-	'receive',
-	'run',
-	'expected'
+	ignoreLog: [ 'space',
 	]
 }).onReady(function(){
 	console.log("Nux ready")
