f60b1d53a69d2818fa71d8c6dc7cbd0169e3e89d
Jay Jagpal <jay@strangemother.com>
2013-11-18 22:07:15 +0000

    Boot loader
    + ljs asset
    + bootloader method chaining

--- src/js/nux/nux.js
+++ src/js/nux/nux.js
@@ -1,148 +1,150 @@
 //https://github.com/malko/l.js
-(function(){
-/*
-* script for js/css parallel loading with dependancies management
-* @author Jonathan Gotti < jgotti at jgotti dot net >
-* @licence dual licence mit / gpl
-* @since 2012-04-12
-* @todo add prefetching using text/cache for js files
-* @changelog
-*            - 2013-01-25 - add parrallel loading inside single load call
-*            - 2012-06-29 - some minifier optimisations
-*            - 2012-04-20 - now sharp part of url will be used as tag id
-*                         - add options for checking already loaded scripts at load time
-*            - 2012-04-19 - add addAliases method
-*/
-	/** gEval credits goes to my javascript idol John Resig, this is a simplified jQuery.globalEval */
-	var gEval = function(js){ ( window.execScript || function(js){ window[ "eval" ].call(window,js);} )(js); }
-		, isA =  function(a,b){ return a instanceof (b || Array);}
-		//-- some minifier optimisation
-		, D = document
-		, getElementsByTagName = 'getElementsByTagName'
-		, replace = 'replace'
-		, match = 'match'
-		, length = 'length'
-		, readyState = 'readyState'
-		, onreadystatechange = 'onreadystatechange'
-		//-- get the current script tag for further evaluation of it's eventual content
-		,scripts = D[getElementsByTagName]("script")
-		,script  = scripts[ scripts[length] - 1 ].innerHTML[replace](/^\s+|\s+$/g,'')
-	;
-	//avoid multiple inclusion to override current loader but allow tag content evaluation
-	if( typeof ljs !== 'undefined' ){ script && gEval(script); return; }
+	(function(){
+	/*
+	* script for js/css parallel loading with dependancies management
+	* @author Jonathan Gotti < jgotti at jgotti dot net >
+	* @licence dual licence mit / gpl
+	* @since 2012-04-12
+	* @todo add prefetching using text/cache for js files
+	* @changelog
+	*            - 2013-01-25 - add parrallel loading inside single load call
+	*            - 2012-06-29 - some minifier optimisations
+	*            - 2012-04-20 - now sharp part of url will be used as tag id
+	*                         - add options for checking already loaded scripts at load time
+	*            - 2012-04-19 - add addAliases method
+	*/
+		/** gEval credits goes to my javascript idol John Resig, this is a simplified jQuery.globalEval */
+		var gEval = function(js){ ( window.execScript || function(js){ window[ "eval" ].call(window,js);} )(js); }
+			, isA =  function(a,b){ return a instanceof (b || Array);}
+			//-- some minifier optimisation
+			, D = document
+			, getElementsByTagName = 'getElementsByTagName'
+			, replace = 'replace'
+			, match = 'match'
+			, length = 'length'
+			, readyState = 'readyState'
+			, onreadystatechange = 'onreadystatechange'
+			//-- get the current script tag for further evaluation of it's eventual content
+			,scripts = D[getElementsByTagName]("script")
+			,script  = scripts[ scripts[length] - 1 ].innerHTML[replace](/^\s+|\s+$/g,'')
+		;
+		//avoid multiple inclusion to override current loader but allow tag content evaluation
+		if( typeof ljs !== 'undefined' ){ script && gEval(script); return; }
 
-	var checkLoaded = scripts[ scripts[length] - 1 ].src[match](/checkLoaded/)?true:false
-		//-- keep trace of header as we will make multiple access to it
-		,header  = D[getElementsByTagName]("head")[0] || D.documentElement
-		,appendElmt = function(type,attrs,cb){
-			var e = D.createElement(type), i;
-			if( cb ){ //-- this is not intended to be used for link
-				if(e[readyState]){
-					e[onreadystatechange] = function(){
-						if (e[readyState] === "loaded" || e[readyState] === "complete"){
-							e[onreadystatechange] = null;
-							cb();
+		var checkLoaded = scripts[ scripts[length] - 1 ].src[match](/checkLoaded/)?true:false
+			//-- keep trace of header as we will make multiple access to it
+			,header  = D[getElementsByTagName]("head")[0] || D.documentElement
+			,appendElmt = function(type,attrs,cb){
+				var e = D.createElement(type), i;
+				if( cb ){ //-- this is not intended to be used for link
+					if(e[readyState]){
+						e[onreadystatechange] = function(){
+							if (e[readyState] === "loaded" || e[readyState] === "complete"){
+								e[onreadystatechange] = null;
+								cb();
+							}
+						};
+					}else{
+						e.onload = cb;
+					}
+				}
+				for( i in attrs ){ e[i]=attrs[i]; }
+				header.appendChild(e);
+				// return e; // unused at this time so drop it
+			}
+			,load = function(url,cb,p){
+
+				p = (p)? this.path(p): '';
+			
+				if( this.aliases && this.aliases[url] ){
+					var args = this.aliases[url].slice(0);
+					isA(args) || (args=[args]);
+					cb && args.push(cb);
+					return this.load.apply(this,args);
+				}
+				if( isA(url) ){ // parallelized request
+					for( var l=url.length; l--;){
+						this.load(url[l]);
+					}
+					cb && url.push(cb); // relaunch the dependancie queue
+					return this.load.apply(this,url);
+				}
+				if( url[match](/\.css\b/) ){
+					return this.loadcss(url,cb);
+				}
+
+
+				return this.loadjs(url,cb);
+			}
+			,loaded = {}  // will handle already loaded urls
+			,loader  = {
+				aliases:{}
+				,loadjs: function(url,cb){
+					url = ljs.path() + url;
+					var id  =(url[match]('#')?url[replace](/^[^#]+#/,''):null);
+					id && (url = url[replace](/#.*$/,''));
+					if( loaded[url] === true ){ // already loaded exec cb if any
+						cb && cb();
+						return this;
+					}else if( loaded[url]!== undefined ){ // already asked for loading we append callback if any else return
+						if( cb ){
+							loaded[url] = function(ocb,cb){ return function(){ ocb && ocb(); cb && cb(); } }(loaded[url],cb);
 						}
-					};
-				}else{
-					e.onload = cb;
+						return this;
+					}
+					// first time we ask this script
+					loaded[url] = function(cb){ return function(){loaded[url]=true; cb && cb();}}(cb);
+					appendElmt('script',{type:'text/javascript',src:url,id:id},function(){ loaded[url]() });
+					return this;
 				}
-			}
-			for( i in attrs ){ e[i]=attrs[i]; }
-			header.appendChild(e);
-			// return e; // unused at this time so drop it
-		}
-		,load = function(url,cb,p){
-
-			p = (p)? this.path(p): '';
-		
-			if( this.aliases && this.aliases[url] ){
-				var args = this.aliases[url].slice(0);
-				isA(args) || (args=[args]);
-				cb && args.push(cb);
-				return this.load.apply(this,args);
-			}
-			if( isA(url) ){ // parallelized request
-				for( var l=url.length; l--;){
-					this.load(url[l]);
-				}
-				cb && url.push(cb); // relaunch the dependancie queue
-				return this.load.apply(this,url);
-			}
-			if( url[match](/\.css\b/) ){
-				return this.loadcss(url,cb);
-			}
-
-
-			return this.loadjs(url,cb);
-		}
-		,loaded = {}  // will handle already loaded urls
-		,loader  = {
-			aliases:{}
-			,loadjs: function(url,cb){
-				url = ljs.path() + url;
-				var id  =(url[match]('#')?url[replace](/^[^#]+#/,''):null);
-				id && (url = url[replace](/#.*$/,''));
-				if( loaded[url] === true ){ // already loaded exec cb if any
+				,loadcss: function(url,cb){
+					var id  =(url[match]('#')?url[replace](/^[^#]+#/,''):null);
+					id && (url = url[replace](/#.*$/,''));
+					if(! loaded[url]){
+						appendElmt('link',{type:'text/css',rel:'stylesheet',href:url,id:id},function(){ loaded[url]=true; });
+					}
+					loaded[url] = true;
 					cb && cb();
 					return this;
-				}else if( loaded[url]!== undefined ){ // already asked for loading we append callback if any else return
-					if( cb ){
-						loaded[url] = function(ocb,cb){ return function(){ ocb && ocb(); cb && cb(); } }(loaded[url],cb);
+				}
+				,load: function(){
+					var argv=arguments,argc = argv[length];
+					if( argc === 1 && isA(argv[0],Function) ){
+						argv[0]();
+						return this;
+					}
+					load.call(this,argv[0], argc <= 1 ? undefined : function(){ loader.load.apply(loader,[].slice.call(argv,1))} )
+					return this;
+				}
+				,addAliases:function(aliases){
+					for(var i in aliases ){
+						this.aliases[i]= isA(aliases[i]) ? aliases[i].slice(0) : aliases[i];
 					}
 					return this;
 				}
-				// first time we ask this script
-				loaded[url] = function(cb){ return function(){loaded[url]=true; cb && cb();}}(cb);
-				appendElmt('script',{type:'text/javascript',src:url,id:id},function(){ loaded[url]() });
-				return this;
-			}
-			,loadcss: function(url,cb){
-				var id  =(url[match]('#')?url[replace](/^[^#]+#/,''):null);
-				id && (url = url[replace](/#.*$/,''));
-				if(! loaded[url]){
-					appendElmt('link',{type:'text/css',rel:'stylesheet',href:url,id:id},function(){ loaded[url]=true; });
+				,path: function(path) {
+					if(path) this._path = path;
+					return this._path || ''
 				}
-				loaded[url] = true;
-				cb && cb();
-				return this;
 			}
-			,load: function(){
-				var argv=arguments,argc = argv[length];
-				if( argc === 1 && isA(argv[0],Function) ){
-					argv[0]();
-					return this;
-				}
-				load.call(this,argv[0], argc <= 1 ? undefined : function(){ loader.load.apply(loader,[].slice.call(argv,1))} )
-				return this;
+		;
+		if( checkLoaded ){
+			var i,l,links;
+			for(i=0,l=scripts[length];i<l;i++){
+				loaded[scripts[i].src]=true;
 			}
-			,addAliases:function(aliases){
-				for(var i in aliases ){
-					this.aliases[i]= isA(aliases[i]) ? aliases[i].slice(0) : aliases[i];
-				}
-				return this;
-			}
-			,path: function(path) {
-				if(path) this._path = path;
-				return this._path || ''
+			links = D[getElementsByTagName]('link');
+			for(i=0,l=links[length];i<l;i++){
+				(links[i].rel==="stylesheet" || links[i].type==='text/css') && (loaded[links[i].href]=true);
 			}
 		}
-	;
-	if( checkLoaded ){
-		var i,l,links;
-		for(i=0,l=scripts[length];i<l;i++){
-			loaded[scripts[i].src]=true;
-		}
-		links = D[getElementsByTagName]('link');
-		for(i=0,l=links[length];i<l;i++){
-			(links[i].rel==="stylesheet" || links[i].type==='text/css') && (loaded[links[i].href]=true);
-		}
-	}
-	//export ljs
-	ljs = loader;
-	// eval inside tag code if any
-	script && gEval(script);
-})();
+		//export ljs
+		ljs = loader;
+		// eval inside tag code if any
+		script && gEval(script);
+	})();
+
+
 /* 
  * zoe.js 0.0.1
  * http://zoejs.org
@@ -1096,6 +1098,7 @@
 
 
 
+
 (function(config){
 	var NuxConfig = {
 		allowGlobals: true,
@@ -1184,9 +1187,7 @@
 					space['_meta'] = {
 							
 						}
-				}
-			
-				
+				}				
 
 				var overrides = Nux.signature.overridesAllowed(name);
 				// Overrides will have been previously
@@ -1854,11 +1855,15 @@
 					required = (ex && ex.required)? ex.required: null,
 					assets = (ex && ex.assets)? ex.assets: null;
 					map = (ex && ex.map)? ex.map: null;
-				
+
+				if( listener.item ) {
+					Nux.bootloader.wrap(listener.item);
+				}
 
 				if(required) {
 					Nux.use(required, function(){
-						console.log('All imports for', listener.name);
+						// Boot should be allowed.
+						listener.item.boot('requires');
 					});
 				}
 
@@ -1895,11 +1900,11 @@
 
 							if(_mapMethod) {
 								var importItem = Import(item);
-								console.log('override', method);
-								debugger;
-								importItem[method] = zoe.fn([importItem[method]])
+								// console.log('override', method);
+							
+								zoe.fn([importItem[method]], _mapMethod)
 								// zoe.fn(importItem, method, _mapMethod);
-								console.log('override', importItem)								
+								// console.log('override', importItem)								
 							}
 						}
 						// chain the methods into the mapping
@@ -1908,6 +1913,7 @@
 
 				if(assets) {
 					var assetsLoaded = (function(listener){
+
 						// This method will be called 
 						// when the assets have been loaded. If no
 						// assets are required, it will be called
@@ -1915,14 +1921,9 @@
 						var ext = listener.item
 						return function(){
 							ext._meta.assetsLoaded = true;
-							console.log("Assets have been loaded", ext);
-							
-							if(ex && ex.hasOwnProperty('main')) {
-								ex.main();
-								ex.booted = true;
-							}
-
+							ext.boot('assets');
 						}
+
 					})(listener);
 					ex.assetsLoaded = false
 					Nux.assets.load(assets, assetsLoaded);
@@ -1968,24 +1969,92 @@
 				};
 
 				// run the extension boot method (main|run) 
-				if(ex && ex.hasOwnProperty('main')) {
-					ex.booted = false;
-					if(ex.assetsLoaded == true) {
-						ex.main();
-						ex.booted = true;
-					}
-				}
-
+				
 				// Call each handler in the array
   				for (var i = 0; i < handlers.length; i++) {
 					var hook = handlers[i];
 					hook.apply(Nux, handler.extensions);
 				};
 			},
+		},
 
+		bootloader: {
+			/*
+			booting and boot handlers ensure extensions and
+			required extensions are booted in a queue.
+			 */
+			boots: {},
 
+			wrap: function(ext){
+				/*
+				Wrap the object into a boot sequence 
+				setup with an endpoint inject into the
+				extension configured to run the main method
+				at the correct time
+				 */
+				var bootSpace = ( Nux.bootloader.boots.hasOwnProperty(ext.name) )? 
+									Nux.bootloader.boots[ext.name]: null;
+				
+				if( !Nux.bootloader.boots[ext.name] ) {
+					bootSpace = Nux.bootloader.boots[ext.name] = {
+						requires: true,
+						assets: true,
+						init: false
+					}
+				}
 
+				if(ext._meta) {	
+					if( ext._meta.required ) {
+						bootSpace.requires = false;
+					}
 
+					if( ext._meta.assets ) {
+						bootSpace.assets = false;
+					}
+				}
+
+				bootFunction = (function(bootSpace){
+				
+					return function(loadVal){
+						var val = arg(arguments, 1, true);
+						var extBootSpace = bootSpace;
+						extBootSpace[loadVal] = val;
+						
+						var tv = true;
+						for(var x in extBootSpace) {
+							if( extBootSpace[x] === false ) {
+								tv = false;
+							}
+						}
+						
+						if( tv ) {
+							// All components for boot have been flagged true.
+							// proceed to call the main boot method.
+							if( ext._meta && 
+								ext._meta.hasOwnProperty('main') && 
+								!extBootSpace.booted
+								) {
+
+								
+								ext._meta.main();
+								extBootSpace.booted = true;
+							
+
+							}
+						}
+						
+					}
+
+				}).apply(ext, [bootSpace]);
+
+				ext.boot = bootFunction;
+				// Calls the boot function and persists to 
+				// run the method. The 'init' is mearly to 
+				// shim a value to change and to provide some 
+				// context of sequence for later event listeners.
+				ext.boot('init')
+				return ext
+			}
 		},
 
 		signature: {
