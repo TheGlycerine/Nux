9dfad450650d1ccbb638f7d672bc86466846f033
Jay Jagpal <jay@strangemother.com>
2013-11-26 02:13:23 +0000

    conceptual meta() method

--- src/js/nux/requires/NuxComponentHeader.js
+++ src/js/nux/requires/NuxComponentHeader.js
@@ -1,7 +1,7 @@
 // This shared resource should be implemented as the
 // required components header within the boot.
 // It's currently here to assist in development ease.
-NuxComponentHeader = function(){
+NuxComponentHeader = function __self(){
 
 	if(!window.hasOwnProperty('Nux')) {
 		
@@ -13,13 +13,41 @@ NuxComponentHeader = function(){
 		       obj[key] = {}
 		     obj = obj[key];
 		   }
+
 		   obj[keyPath[lastKeyIndex]] = value;
 		   return obj
 		}
 
 		NuxImplement(arguments);
 	} else {
-		Nux.implement(arguments);
+		Nux.implement.apply(this, arguments);
 	}
+
+	// Return an entity used to extend this after import
+	return (function(){
+		var options = arg(arguments, 0, {});
+		var method = arg(arguments, 1, null);
+		
+		return {
+			options: options,
+			method: method,
+			chain: function(map, obj) {
+				return NuxComponentHeader.apply(this, arguments)
+
+			},
+			meta: function(name, v){
+				/*
+				Apply meta data about your nux component to add
+				context when in use
+				 */
+				if(!window.hasOwnProperty('Nux')) {
+					console.error('meta method not implemented')
+				} else {
+					Nux.errors.throw(04, 'meta', name);
+				}
+			}
+		};
+
+	}).apply(this, arguments);
 
 };
