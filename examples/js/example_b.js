48c3b203a9e307d4fda37e4973e43798a7382974
Jay Jagpal <jay@strangemother.com>
2013-12-05 23:58:27 +0000

    Better examples
    + import example file to simply Nux example
    + console output on file example_b

--- examples/js/example_b.js
+++ examples/js/example_b.js
@@ -1,25 +1,3 @@
-$(document).ready(function(){
-	
-	$('button.import').click(function(){
-		var self = $(this);
-		Nux.use(self.text(), function(){
-			self.addClass('imported');
-		})
-	})
-
-	$('.switch').click(function(){
-		localStorage.theme = $('.switch input')[0].checked
-		$('body').toggleClass('dark', $('.switch input')[0].checked);
-		
-	})
-
-	if(localStorage.theme == 'true') {
-		$('body').addClass('dark');
-		$('.switch input')[0].checked = true
-	}
-
-})
-
 nuxReady = function(){
 	// This method will boot when nux is prepared
 
@@ -31,42 +9,8 @@ nuxReady = function(){
 		car.lock()
 
 	}, 'js/exampleb/car_parts/');
-
-
+ 
 	_human = Nux.use(['human.body'], function(){
 		console.log(human);
-	}, 'js/exampleb/human_parts/')
-
-	
+	}, 'js/exampleb/human_parts/')	
 }
-
-// Will load Nux or the development environment.
-bootNux = function(){
-	if(!window.hasOwnProperty('Nux')) return false
-	if(window['__devInt']) window.clearInterval(window['__devInt']);
-
-	;Nux
-	// First the boot, applying a set of rules to
-	// boot your nux object.
-	.boot({
-		// Boot a full Nux system complete with batteries.
-		// refer to nux.extension.kernel
-		kernel: false,
-		// console logs and test are implemented if required.
-		debug: false,
-		// Asset path loads the files required for the extension
-		// from this location. This should map to your
-		// JS folder relative to the location of this
-		// boot config
-		assetPath: '../src/js/nux/assets',
-		// Load folder for extension matching the expression
-		extensionPath: "../src/js/nux/extensions/",
-		vendorPath: "../src/js/vendor",
-		required: false,
-	}).onReady(function(){
-		nuxReady()
-	});
-
-	return true
-}
-window['__devInt'] = (!bootNux())? window.setInterval(bootNux, 100): -1;
---
+++ examples/js/examples.js
@@ -0,0 +1,51 @@
+/*
+Shared across all example files to clean up Nux examples and to make it easier to
+share the template.
+ */
+$(document).ready(function(){
+	
+	$('.switch').click(function(){
+		localStorage.theme = $('.switch input')[0].checked
+		$('body').toggleClass('dark', $('.switch input')[0].checked);
+		
+	})
+
+	if(localStorage.theme == 'true') {
+		$('body').addClass('dark');
+		$('.switch input')[0].checked = true
+	}
+
+})
+
+
+// Will load Nux or the development environment.
+bootNux = function(){
+	if(!window.hasOwnProperty('Nux')) return false
+	if(window['__devInt']) window.clearInterval(window['__devInt']);
+
+	;Nux
+	// First the boot, applying a set of rules to
+	// boot your nux object.
+	.boot({
+		// Boot a full Nux system complete with batteries.
+		// refer to nux.extension.kernel
+		kernel: false,
+		// console logs and test are implemented if required.
+		debug: false,
+		// Asset path loads the files required for the extension
+		// from this location. This should map to your
+		// JS folder relative to the location of this
+		// boot config
+		assetPath: '../src/js/nux/assets',
+		// Load folder for extension matching the expression
+		extensionPath: "../src/js/nux/extensions/",
+		vendorPath: "../src/js/vendor",
+		required: false,
+	}).onReady(function(){
+		nuxReady()
+	});
+
+	return true
+}
+window['__devInt'] = (!bootNux())? window.setInterval(bootNux, 100): -1;
+
