0c9931056765a8339919cca3bd278ae5607bf60d
Jay Jagpal <jay@strangemother.com>
2013-12-06 21:54:14 +0000

    created examples

--- examples/js/example_c.js
+++ examples/js/example_c.js
@@ -1,8 +1,30 @@
+examples = {
+	importShip: function(button){
+		// Ship is imported
+		Nux.use(['spaceship'], function(){
+			console.log('spaceship has loaded');
+			$(button).addClass('imported');
+		}, 'js/examplec/');
+	},
+	importRenderer: function(button) {
+		Nux.use(['spaceship.renderer'], function(){
+			console.log('spaceship renderer');
+			$(button).addClass('imported');
+		}, 'js/examplec');
+	}
+}
+
+$(document).ready(function(){
+	$('#spaceshipButton').click(function(){
+		examples.importShip(this)
+	});
+
+	$('#spaceshipRendererButton').click(function(){
+		examples.importRenderer(this)
+	});
+
+});
+
 nuxReady = function(){
 	// This method will boot when nux is prepared
-
-	// lets build a spaceship!
-	_nuxcar = Nux.use(['spaceship'], function(){
-		console.log('spaceship has loaded')
-	}, 'js/examplec/')
 }
